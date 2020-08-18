3cb4b984c5052410ad5bad9086cda34733053f18
Adona Tesfaye <adonatesfaye99@gmail.com>
2020-08-18 15:22:38 +0300

    We have separated out our front-end client from our back-end express server that handles our stripe charges. In this section we learn how to write our express backend server as well as syncing our client to request our backend to complete a stripe charge!

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,14 +1,26 @@
 import React from "react"
 import StripeCheckout from 'react-stripe-checkout'
+import axios from "axios"
 
-const onToken=token=>{
-    console.log(token)
-    alert('Payment Successful')
-}
 
 const StripeCheckoutButton=({price})=>{
     const priceForStrip=price*100;
     const publishablekey="pk_test_51GxWguHicZ0dFbvUxrIjUH6XvhpSiOERMkuwYgG49pIlLDK6F9CklV4z3GRjIoNgbCAP7vS1gEdaxl6M6SsosnjP002ZUPnDck";
+    const onToken=token=>{
+        axios({
+                url:"payment",
+                method:"post",
+                data:{
+                    amount:priceForStrip,
+                    token
+                }
+            }).then(response=>{
+                alert('Payment Successful');
+            }).catch(error=>{
+                alert('There was an issue with your payment. Please make sure you use the provided credit card');
+                console.log("payment error: ",JSON.parse(error));
+            });
+    }
   return (
       <StripeCheckout 
       label="Pay Now"
